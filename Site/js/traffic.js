808c97e8ec95910479d9d029ed6c5f20a6982a99
RainerNet <93415390+RainerNet@users.noreply.github.com>
2021-08-25 07:23:29 +0000

    Added an icon

---
+++ Site/js/traffic.js
@@ -0,0 +1,25 @@
+
+
+
+HHH = {};
+HHH.Data = {};
+///URL
+
+
+//Object Methods
+
+HHH.LoadGame = function(){
+	HHH.PopulatePage(1);	
+}
+
+HHH.OptionClick = function(){
+	var nextQuestionID = $(this).id().split("_")[1];
+	
+}
+
+
+HHH.GetData = function(){
+	$.ajax({
+		url: "http://izzycjohnston.com/gdihhh/php/cit_json.php",
+		dataType: "json",
+		crossDomain: "true"
