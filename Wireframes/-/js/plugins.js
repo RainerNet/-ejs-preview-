8b0c95e6f1559e12934770488563e884852a704e
RainerNet <93415390+RainerNet@users.noreply.github.com>
2022-10-01 21:19:07 +0000

    add override

---
+++ Wireframes/-/js/plugins.js
@@ -0,0 +1,7 @@
+window.log = function(){
+  log.history = log.history || [];
+  log.history.push(arguments);
+  arguments.callee = arguments.callee.caller;  
+  if(this.console) console.log( Array.prototype.slice.call(arguments) );
+};
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,e
